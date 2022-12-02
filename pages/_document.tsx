1f84916796b6202b46e5e13d38d17b08398f4019
timurkaev <timurkaev.is@gmail.com>
2022-12-02 17:16:53 +0300

    create _document file

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+const Document = (): JSX.Element => {
+	return (
+		<Html lang="ru">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	);
+};
+
+export default Document;
